b99bbf7988171a95ff1e0b665164ca956c85fb6e
Vukasin Macura <moj_email@gmail.com>
2020-09-29 19:18:07 +0200

    sfds

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AnimalList msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import AnimalList from './components/AnimalList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AnimalList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AnimalList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="hello">
+    
+    <p>Lista Zivotinja:</p>
+  <div class="table">
+    <table>
+      <tr> 
+        <td>Vrsta</td>
+        <td>Ime</td>
+        <td>Datum</td>
+      </tr>
+      <tr v-for="(animal,index) in animals" :key="index">
+        <td>{{ animal.vrsta }}</td>
+        <td>{{ animal.name }}</td>
+        <td>{{ animal.datumRodjenja }}</td>
+      </tr>
+    </table>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data () {
+  return {
+  
+  animals: [
+        { vrsta: "Pas", name: "Mica", datumRodjenja: new Date().toLocaleString() },
+        { vrsta: "Medved", name: "Balu", datumRodjenja: new Date().toLocaleString() },
+        { vrsta: "Oro", name: "Mimi", datumRodjenja: new Date().toLocaleString() },
+        { vrsta: "Delfin", name: "Pero", datumRodjenja: new Date().toLocaleString() },
+        { vrsta: "Macka", name: "Cile ", datumRodjenja: new Date().toLocaleString() }
+  ],
+
+  
+  }
+  }  
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+
+table td {
+width: 100px;
+}
+
+table {
+  border-collapse: collapse;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+tr:hover {background-color: green;
+  cursor: pointer;
+}
+</style>
